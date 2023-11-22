26b428769ab0cc6e073b0f2bbc113d9f62ae55b4
anilkumawat7788 <anilkumawat791@gmail.com>
2023-11-22 15:15:21 +0530

    react version changes agian again gjdsddasdasdadwdwdwe jhfsjdf

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import './App.css';
 //   Route,
 //   Navigate,
 // } from "react-router-dom";
-import { Routes, Route, Link } from "react-router-dom";
+// import { Routes, Route, Link } from "react-router-dom";
+
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Redirect
+// } from "react-router-dom";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/aboutus/About';
 import Login from './pages/auth/login/Login';
@@ -15,36 +24,53 @@ import Login from './pages/auth/login/Login';
 function App() {
   // const user = localStorage.getItem("user");
 
-  // const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
-  // useEffect(() => {
-  //   // Check if the user is already logged in (stored in localStorage)
-  //   const storedUser = localStorage.getItem('user');
-  //   if (storedUser) {
-  //     setUser(JSON.parse(storedUser));
-  //   }
-  // }, []);
+  useEffect(() => {
+    // Check if the user is already logged in (stored in localStorage)
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
-//  console.log("user is", user)
+ console.log("user is", user)
   return (
     <>
-
-
         {/* <Routes>
-          <Route exact path="/" element={user ? <Home /> : <Login />} />
-          <Route exact path="/about" element={user ? <About /> : <Login />} />
-          <Route exact path="/login" element={user ? <Home /> : <Login />} />
-          <Route exact path="/verify-otp" element={ user ? <Home /> : <OtpVerify /> } />
+          <Route exact path="/" element={  <PrivateRoute>  <About /> </PrivateRoute> } />
+          <Route exact path="/about" element={ <About /> } />
+           <Route exact path="/login" element={ <Login />} />
+          <Route exact path="/verify-otp" element={  <OtpVerify /> } />
         </Routes> */}
    
-
-<Routes>
+        <Router>
+      {/* Your other components */}
+      <Routes>
+      <Route path="/login" element={<Login />} />
+        <Route exact path="/about" element={  <PrivateRoute>  <About /></PrivateRoute> } />
+        <Route path="/" element={ <PrivateRoute> <Home /> </PrivateRoute> } />
+        <Route exact path="/verify-otp" element={ <OtpVerify />  } />
+         <Route path="*" element={<Login />} />
+        {/* Other routes */}
+      </Routes>
+    </Router>
+{/* <Routes>
         <Route path="/login" element={<Login />} />
         <Route exact path="/about" element={  <PrivateRoute>  <About /></PrivateRoute> } />
         <Route path="/" element={ <PrivateRoute> <Home /> </PrivateRoute> } />
         <Route exact path="/verify-otp" element={ <OtpVerify />  } />
          <Route path="*" element={<Login />} />
-      </Routes>
+      </Routes> */}
+
+{/* <Routes>
+      <Switch>
+        <Route exact path="/">
+        { <PrivateRoute> <Home /> </PrivateRoute> }
+        </Route>
+        </Switch>
+        </Routes> */}
+        
     </>
   );
 }
